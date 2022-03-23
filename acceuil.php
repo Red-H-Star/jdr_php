cd9e29004d0a287128eae55c36eebe6bcdda0815
ALEXANDRE TRUCHON <truchona@fenelonsio.local>
2022-03-23 16:17:04 +0100

    initial commit

---
+++ acceuil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JDR en ligne</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Hello there !</h1>
+        <h1>General Kenobi<h1>
+    </body>
+</html>
